c9fd3b0cc6e436ba3c00e0100dc1dd9c068e30fb
YODA\panhe <kristithecoder@gmail.com>
2017-05-08 17:43:43 -0700

    working on data structure

--- src/List.ts
+++ src/List.ts
@@ -10,13 +10,33 @@ class List<T> {
         //TODO:ensure type safety
         this._list = list;
     }
+    function isValidList(List: T[]){
+         list.forEach(function(entry) {
+        if (typeof entry !== 'object') {
+            valid = false;
+        }
+    });
+    if (valid != undefined)
+        return false;
+    if (list.length>0) {
+        var propNames = JSON.stringify(Object.getOwnPropertyNames(list[0]));
+        for (var i = 1; i < list.length; i++) {
+            var objectPropNames = JSON.stringify(Object.getOwnPropertyNames(list[i]));
+            if (objectPropNames !== propNames) {
+                return false;
+            }
+
+        }
+    }
+    return true;
+    }
     //functions:
     //isValidList(list:T[]):bool
     //getValidationErrors(list:T[]):string[]
     //sort(sublist:T[],properties:string[], asc:bool):T[]
-    //search(subulist:T[], target:string, properties:string[])T[]
+    //search(sublist:T[], target:string, properties:string[])T[]
     addRange(list: T[]): boolean {
-        return false;
+        if(isValidSublist(list))
     }
 
 
