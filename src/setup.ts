8b1e11b9174110172d3390de99209a835e81dca7
theKristi <kristithecoder@gmail.com>
2017-05-13 20:04:09 -0700

    Working on getting thuings set up

--- src/setup.ts
+++ src/setup.ts
@@ -1,12 +1,17 @@
 ﻿import { TableParser } from "./TableParserAsync";
+import { List } from "./List"
 document.addEventListener("DOMContentLoaded", function (event) {
     //do work
     //tableViews = [];
+
+    //var List=new List();
     var parser= new TableParser();
     let tables:HTMLCollectionOf<HTMLTableElement> = document.querySelectorAll("[data-list-manipulate]") as HTMLCollectionOf<HTMLTableElement>;
     for (var i = 0; i < tables.length; i++) {
-        parser.parseFromHtml(tables[i] as any, 10000, function(data){
-        	console.log(data);
+    	var table:HTMLTableElement=tables[i] as HTMLTableElement;
+        parser.parseFromHtml(table, 10000, function(data){
+        	console.log("parsed");
+        	table.classList.remove("hidden");
         });
     }
     
